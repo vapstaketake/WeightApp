8a462d2101feba136bfee63fc961d91db69081aa
cappuccino3044vaps <vapsok299@gmail.com>
2025-04-13 22:05:26 +0900

    fix visualizer

--- Weight_Sensor/visualizer/header.py
+++ Weight_Sensor/visualizer/header.py
@@ -10,10 +10,4 @@ if is_windows:
 else:
     EXECUTABLE_PATH=os.path.join(os.path.dirname(os.path.abspath(__file__)), "weight_reader")
 
-for i in range(10):
-    # 生の読み取り値を取得
-    weight = HX711.get_raw_reading(EXECUTABLE_PATH)
-    if weight is not None:
-        print(f"Raw weight reading: {weight} g")
-    else:
-        print("Failed to get weight reading.")
+
