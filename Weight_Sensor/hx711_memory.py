0ce5de732b7aaaac5f8fe0de466b2f28d1cb860b
cappuccino3044vaps <vapsok299@gmail.com>
2025-04-13 23:55:27 +0900

    実行ファイルを起動するように変更

--- Weight_Sensor/hx711_memory.py
+++ Weight_Sensor/hx711_memory.py
@@ -4,6 +4,16 @@ import time
 import posix_ipc
 import subprocess
 import os
+import os
+import sys
+
+
+is_windows = sys.platform.startswith('win')
+if is_windows:
+    # Windows環境の場合
+    EXECUTABLE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "weight_reader.exe")
+else:
+    EXECUTABLE_PATH=os.path.join(os.path.dirname(os.path.abspath(__file__)), "weight_reader")
 
 SHM_NAME = "/weight_shm"
 
@@ -12,7 +22,7 @@ class SensorData(ctypes.Structure):
         ("weight", ctypes.c_double),
         ("ready", ctypes.c_bool)
     ]
-process = subprocess.Popen(["./sensor_writer"])
+process = subprocess.run([EXECUTABLE_PATH], capture_output=True, text=True, check=True)
 memory=posix_ipc.SharedMemory(SHM_NAME)
 map_file=mmap.mmap(memory.fd,ctypes.sizeof(SensorData),mmap.MAP_SHARED, mmap.PROT_READ | mmap.PROT_WRITE)
 try:
@@ -24,7 +34,7 @@ try:
             print(f"Weight: {weight} g")
             sensor_data.ready=False
         time.sleep(0.5)
-        
+
 except KeyboardInterrupt:
     print("closed")
 
