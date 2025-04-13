812c0b4f436f280e06f21d0caf7cb09b719c9b99
cappuccino3044vaps <vapsok299@gmail.com>
2025-04-13 23:53:38 +0900

    実行ファイルを起動するように変更

--- Weight_Sensor/hx711_memory.py
+++ Weight_Sensor/hx711_memory.py
@@ -2,6 +2,8 @@ import mmap
 import ctypes
 import time
 import posix_ipc
+import subprocess
+import os
 
 SHM_NAME = "/weight_shm"
 
@@ -10,16 +12,23 @@ class SensorData(ctypes.Structure):
         ("weight", ctypes.c_double),
         ("ready", ctypes.c_bool)
     ]
-
+process = subprocess.Popen(["./sensor_writer"])
 memory=posix_ipc.SharedMemory(SHM_NAME)
 map_file=mmap.mmap(memory.fd,ctypes.sizeof(SensorData),mmap.MAP_SHARED, mmap.PROT_READ | mmap.PROT_WRITE)
-memory
+try:
+    while True:
+        map_file.seek(0)
+        sensor_data = SensorData.from_buffer(map_file)
+        if sensor_data.ready:
+            weight = sensor_data.weight
+            print(f"Weight: {weight} g")
+            sensor_data.ready=False
+        time.sleep(0.5)
+        
+except KeyboardInterrupt:
+    print("closed")
 
-sensor_data=SensorData.from_buffer(map_file)
-
-while True:
-    if sensor_data.ready:
-        weight = sensor_data.weight
-        print(f"Weight: {weight} g")
-        sensor_data.ready=False
-    time.sleep(0.1)
+finally:
+    map_file.close()
+    memory.close()
+    process.terminate()
